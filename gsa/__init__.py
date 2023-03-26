ede24a3c9a87eb8542a6cd33ee5dbc86096a4db9
Ethan Bryson <ebryson09@gmail.com>
2023-03-25 20:50:23 -0700

    Updated registering of init-db command

--- gsa/__init__.py
+++ gsa/__init__.py
@@ -1,18 +1,12 @@
 import os
-
 from flask import Flask, session
 
 def create_app(test_config=None):
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_mapping(
 		SECRET_KEY='dev',
-		DATABASE=os.path.join(app.instance_path, 'gsa.db'),
+		DATABASE= os.path.join(app.instance_path, 'gsa.db'),
 	)
-	
-	if test_config is None: #load instance config when not testing
-		app.config.from_pyfile('config.py', silent=True)
-	else:
-		app.config.from_mapping(test_config)
 		
 	try: #ensure instance folder exists
 		os.makedirs(app.instance_path)
@@ -20,10 +14,13 @@ def create_app(test_config=None):
 		pass
 	
 	from . import db
-	db.init_app(app)	
+	db.register_app(app)	
 	
 	from . import auth
 	app.register_blueprint(auth.bp)
+
+	from . import game
+	app.register_blueprint(game.bp)
 	
 	from . import collection
 	app.register_blueprint(collection.bp)
